c78f3e9577bdebc553f519cb4fecabf78cb47826
juanortiz10 <juanjo.jjo94@gmail.com>
2017-08-16 20:03:41 -0500

    First commit

---
+++ mi-primera-app-en-react/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="App-text">Hola Mundo!! </p>
+      </div>
+    );
+  }
+}
+
+export default App;
